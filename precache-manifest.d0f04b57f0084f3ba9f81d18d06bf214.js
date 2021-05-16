dcb1ffc56e386b45584f9cb05f47d085b6955ec5
mdshamoon <mohdshamoon841@gmail.com>
2021-05-16 19:03:40 +0530

    Updates

---
+++ precache-manifest.d0f04b57f0084f3ba9f81d18d06bf214.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2f06505dfc0cbd8d9afb6650f9381c5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1bbe72131c5e8fedafe4",
+    "url": "./static/css/2.ab939ae2.chunk.css"
+  },
+  {
+    "revision": "0aadb3290c1283cfaf31",
+    "url": "./static/css/main.c9cea400.chunk.css"
+  },
+  {
+    "revision": "1bbe72131c5e8fedafe4",
+    "url": "./static/js/2.bd95a062.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "./static/js/2.bd95a062.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0aadb3290c1283cfaf31",
+    "url": "./static/js/main.0280e1c0.chunk.js"
+  },
+  {
+    "revision": "7d08f10027e0938ac6d7",
+    "url": "./static/js/runtime-main.05ee1771.js"
+  }
+]);
